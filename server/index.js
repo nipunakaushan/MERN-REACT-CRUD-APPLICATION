4ffd32a0b182c83606eca3cb21758186263ff50b
Kaushan Jayasundara <57662753+nipunakaushan@users.noreply.github.com>
2023-08-07 02:05:48 +0530

    Completed back end router and controller functions
    Completed back end router and controller functions

--- server/index.js
+++ server/index.js
@@ -12,13 +12,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';  //import posts routes from routs falder
+
 const app = express();
+
+app.use('/posts', postRoutes);
 app.use(bodyParser.json({ limit: "50mb" ,extended: true }));
 app.use(bodyParser.urlencoded({limit:"50mb",extended:true}));
 app.use(cors());
 
-const CONNECTION_URL = ;
+const CONNECTION_URL = 'mongodb+srv://blajkwolf:xnULRyjhsr10EFB6@merncrud0.xtdle4j.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log('Server has started on port 5000')))
   .catch((error) => console.log(error.message));
+  
+
+   
---
+++ server/routes/posts.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { getPosts,createPost } from '../controllers/posts.js';
+
+const router = express.Router();
+// http://localhost:5000/posts
+
+
+router.get('/', getPosts ); // delcare the controller exported function
+router.post('/', createPost );
+export default router;
